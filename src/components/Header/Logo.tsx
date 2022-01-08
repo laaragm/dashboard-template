a60c69a1677ed1b12b77d742dca42e50fc1fe3ea
Lara <laragalvanim@gmail.com>
2022-01-08 12:31:20 -0300

    Adjust Header so it can be responsive

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -2,7 +2,12 @@ import { Text } from "@chakra-ui/react";
 
 export function Logo() {
     return (
-        <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">
+        <Text
+            fontSize={["2xl", "3xl"]} // 2xl for mobile and 3xl onwards
+            fontWeight="bold"
+            letterSpacing="tight"
+            w="64"
+        >
             dashgo
             <Text as="span" ml="1" color="pink.500">
                 .
